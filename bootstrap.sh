85c1310cb8af983102092d88b505488c6f7249b6
Mpho Mphego <mpho112@gmail.com>
2019-02-20 11:50:48 +0200

    Updated bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
 # Bootstrap script
 
+if [ -f ".bash_configs" ]; then
+    wget --quiet https://raw.githubusercontent.com/ska-sa/cbf-jenkins-scripts/master/.bash_configs
+    wget --quiet https://raw.githubusercontent.com/ska-sa/cbf-jenkins-scripts/master/1.install_dependencies.sh
+    wget --quiet https://raw.githubusercontent.com/ska-sa/cbf-jenkins-scripts/master/2.verify_dependencies.sh
+    wget --quiet https://raw.githubusercontent.com/ska-sa/cbf-jenkins-scripts/master/3.initialise_instrument.sh
+    wget --quiet https://raw.githubusercontent.com/ska-sa/cbf-jenkins-scripts/master/4.corr2rx_test.sh
+    wget --quiet https://raw.githubusercontent.com/ska-sa/cbf-jenkins-scripts/master/5.run_tests.sh
+fi
 # shellcheck disable=SC1091
 source .bash_configs
 bash 1.install_dependencies.sh && \
